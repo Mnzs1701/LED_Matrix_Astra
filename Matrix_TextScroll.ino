c848e3d08b6e19c4d7a3cfdda9fea39ce93b717c
Mnzs1701 <43721824+Mnzs1701@users.noreply.github.com>
2020-02-27 22:26:48 +0530

    Add files via upload
    Codes for Arduino and Android app apk

---
+++ Matrix_TextScroll.ino
@@ -0,0 +1,40 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef PSTR
+#define PSTR 
+#endif
+
+#define PIN 6
+#define mw 18
+#define mh 7
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(mw, mh, PIN,
+NEO_MATRIX_ZIGZAG, 
+  NEO_GRB            + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(125, 0, 0), matrix.Color(0, 125, 0), matrix.Color(0, 0, 125) };
+
+void setup() {
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(40);
+  matrix.setTextColor(colors[0]);
+}
+
+int x    = matrix.width();
+int pass = 0;
+
+void loop() {
+  matrix.fillScreen(0);
+  matrix.setCursor(x,0);
+  matrix.print(F("ABCDE abcde"));
+  if(--x < -50) {
+    x = matrix.width();
+    if(++pass >= 3) pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+  matrix.show();
+  delay(70);
+}
